4e30a7f89712451ededec9afb7527e7854564032
Nat <nat@cmmc.io>
2019-01-16 14:40:53 +0700

    update path.

--- Xtensa.js
+++ Xtensa.js
@@ -11,21 +11,21 @@ const ospath = function (p) {
 }
 
 const setConfig = (AppContext) => {
+    const app_dir = `${AppContext.process_dir}/${AppContext.user_app_dir}/${AppContext.board_name}`
     G = Object.assign({}, AppContext)
     G.compiler = AppContext.compiler
     G.Log = require('./log')
     G.esptool = AppContext.esptool
     G.board_name = AppContext.board_name
     G.toolchain_dir = AppContext.toolchain_dir
-    G.user_app_dir = `${AppContext.process_dir}/${AppContext.user_app_dir}/${AppContext.board_name}`
     G.COMPILER_AR = `${G.toolchain_dir}/xtensa-esp32-elf-ar`
     G.COMPILER_GCC = `${G.toolchain_dir}/xtensa-esp32-elf-gcc`
     G.COMPILER_CPP = `${G.toolchain_dir}/xtensa-esp32-elf-c++`
-    G.ELF_FILE = `${G.user_app_dir}/${G.board_name}.elf`
-    G.BIN_FILE = `${G.user_app_dir}/${G.board_name}.bin`
+    G.user_app_dir = app_dir
+    G.ELF_FILE = `${app_dir}/${G.board_name}.elf`
+    G.BIN_FILE = `${app_dir}/${G.board_name}.bin`
     G.ARCHIVE_FILE = `${G.user_app_dir}/libmain.a`
     G.PROCESS_DIR = AppContext.process_dir || `${__dirname}/../..`
-    console.log(G.user_app_dir)
     G.ospath = ospath
     console.log(`process_dir=${G.process_dir}`)
 }
