ada44f47de4a3cc8302c94afce0f7feb587df2c3
Nat <nat@cmmc.io>
2019-01-17 14:59:45 +0700

    remove unused code.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const fs = require('fs')
 const path = require('path')
 const {createCompiler} = require('./Compiler')
@@ -14,21 +13,23 @@ var argv = require('yargs')
             console.log(`please add option --context [file.json]`)
             return
         }
+
         let context = path.resolve(argv.context)
-        // console.log(`${context}, ${fs.existsSync(context)} `)
+        let Compiler
+
         context = JSON.parse(fs.readFileSync(context).toString())
+
         context.process_dir = context.kidbright_path
         context.toolchain_dir = `${context.process_dir}/xtensa-esp32-elf/bin`
         context.esptool = `${context.process_dir}/esptool`
-
-        let Compiler = createCompiler(context)
-
         context.compiler.plugins_sources.push(`${context.user_app_dir}/${context.board_name}/user_app.cpp`)
+
+        Compiler = createCompiler(context)
+
         Compiler.compile(context.compiler).then(() => {
             console.log('compile all files done')
         }).catch(err => {
             console.error("project failed.")
-            // console.error(err)
         })
     })
     .help('help')
@@ -36,7 +37,6 @@ var argv = require('yargs')
     .alias('version', 'v')
     .alias('help', 'h')
     .alias('context', 'c')
-    // .demandOption(['w','h'])
     .showHelpOnFail(true, 'whoops, something went wrong! run with --help')
     .command('flash', 'flash device using esptool.', yargs => {
         // pass
@@ -49,7 +49,7 @@ var argv = require('yargs')
 
         if (!argv.port) {
             console.log(`no port specify.`)
-            return 0
+            return
         } else {
             if (fs.existsSync(argv.port)) {
                 const Xtensa = require('./Xtensa')
@@ -59,13 +59,11 @@ var argv = require('yargs')
             }
         }
     })
-    .command('generate', 'generate dummy config.', yargs => {
+    .command('generate', 'generate a dummy context configuration.', yargs => {
         // pass
     }, argv => {
         const dummy = fs.readFileSync(`${__dirname}/context.json`).toString()
         console.log(dummy)
-        // if (argv._[1] === 'context') {
-        // }
     })
     .demandCommand(1)
     .argv;
